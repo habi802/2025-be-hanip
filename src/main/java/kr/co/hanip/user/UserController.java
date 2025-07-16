26a67003c743879ee98b65bca4a85a58d1e4497e
habi802 <shb8002@gmail.com>
2025-07-16 15:50:50 +0900

    api 호출 시 응답 객체를 유저 부분에 적용

--- src/main/java/kr/co/hanip/user/UserController.java
+++ src/main/java/kr/co/hanip/user/UserController.java
@@ -1,10 +1,13 @@
 package kr.co.hanip.user;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.co.hanip.common.model.ResultResponse;
 import kr.co.hanip.common.util.HttpUtils;
+import kr.co.hanip.user.etc.UserConstants;
 import kr.co.hanip.user.model.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,44 +19,48 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody UserJoinReq req) {
+    public ResponseEntity<ResultResponse<Integer>> join(@RequestBody UserJoinReq req) {
         //log.info("user-post-req: {}", req);
         int result = userService.join(req);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ResultResponse.success(result));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginReq req, HttpServletRequest httpReq) {
+    public ResponseEntity<ResultResponse<UserLoginRes>> login(@RequestBody UserLoginReq req, HttpServletRequest httpReq) {
         UserLoginRes result = userService.login(req);
         if (result == null) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(ResultResponse.fail(401, "아이디나 비밀번호가 올바르지 않습니다."));
         }
 
-        HttpUtils.setSession(httpReq, "LoggedInUserId", result.getId());
+        HttpUtils.setSession(httpReq, UserConstants.LOGGED_IN_USER_ID, result.getId());
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ResultResponse.success(result));
     }
 
     @GetMapping("/check")
-    public ResponseEntity<?> check(HttpServletRequest httpReq) {
-        int result = (int) HttpUtils.getSessionValue(httpReq, "LoggedInUserId");
-        return ResponseEntity.ok(result);
+    public ResponseEntity<ResultResponse<Integer>> check(HttpServletRequest httpReq) {
+        int result = (int) HttpUtils.getSessionValue(httpReq, UserConstants.LOGGED_IN_USER_ID);
+        return ResponseEntity.ok(ResultResponse.success(result));
     }
 
     @GetMapping
-    public ResponseEntity<?> find(HttpServletRequest httpReq) {
-        int loggedInUserId = (int) HttpUtils.getSessionValue(httpReq, "LoggedInUserId");
+    public ResponseEntity<ResultResponse<UserGetRes>> find(HttpServletRequest httpReq) {
+        int loggedInUserId = (int) HttpUtils.getSessionValue(httpReq, UserConstants.LOGGED_IN_USER_ID);
         UserGetRes result = userService.find(loggedInUserId);
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ResultResponse.success(result));
     }
 
     @PutMapping
-    public ResponseEntity<?> update(@RequestBody UserUpdateReq req, HttpServletRequest httpReq) {
-        int loggedInUserId = (int) HttpUtils.getSessionValue(httpReq, "LoggedInUserId");
+    public ResponseEntity<ResultResponse<Integer>> update(@RequestBody UserUpdateReq req, HttpServletRequest httpReq) {
+        int loggedInUserId = (int) HttpUtils.getSessionValue(httpReq, UserConstants.LOGGED_IN_USER_ID);
         Integer result = userService.update(loggedInUserId, req);
         if (result == null) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(ResultResponse.fail(401, "비밀번호가 올바르지 않습니다."));
         }
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ResultResponse.success(result));
     }
 }
