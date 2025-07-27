de2aa682283469509cf5dea2a59d6edc40afba6f
habi <shb8002@gmail.com>
2025-07-27 13:53:54 -0700

    static 빌드를 위한 삭제

--- src/main/java/kr/co/hanip/customer/etc/CustomerJoinConstants.java
+++
@@ -1,6 +0,0 @@
-package kr.co.hanip.customer.etc;
-
-public class CustomerJoinConstants {
-    public static final Integer DUPLICATE_ID = 2;
-    public static final Integer PASSWORD_MISMATCH = 3;
-}
